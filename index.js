bd15bc5fa5f7ba3dbb284c8e355dca81c47b2991
carsonlafave <clafaveaz@gmail.com>
2021-10-21 21:07:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-export function scuberGreetingForFeet(someValue) {
+ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400){
    return 'This one is on me!'; 
-  } else if (someValue > 2000) {
-    return 'I will gladly take your thirty bucks.'; 
-  } else if (someValue > 2500) {
+  }  if (someValue >= 2500) {
     return 'No can do.'; 
+  } else if (someValue >= 2000) 
+    return 'I will glady take your thirty bucks.'; 
   }
-}
 
 
 
@@ -15,14 +14,39 @@ export function scuberGreetingForFeet(someValue) {
 
 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}  
+function ternaryCheckCity(myPlace) {
+  if (myPlace = 1) {
+    var nyc = 1
+    return 'Ok, sounds good.' ;
   
+    } else if (myPlace = 2) 
+
+          return "No go." ;
+     
+}
+
+function switchOnCharmFromTip(money){
+
+  switch(money){
+    case "generous":
+    return  "Thank you so much.";
+        
+      case "not as generous": 
+        return  "Thank you.";
+       
+      default: 
+      return  "Bye.";
+      
+
+
+
+}
+
+
+
    
 
-
+      
+    
+  }
